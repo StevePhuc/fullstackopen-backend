4788f9fdfb97443969b866e1d86431e7497596fc
Steve Phuc <stevephuc.dev@gmail.com>
2019-08-01 10:42:45 +0300

    3.9

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+
+app.use(cors());
+
 let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-    }
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
 ];
 
 app.get("/api/persons", (req, res) => {
-    res.json(persons);
+  res.json(persons);
 });
 app.get("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
+  const id = Number(req.params.id);
 
-    const findPerson = persons.find(person => person.id === id);
-    findPerson ? res.json(findPerson) : res.status(404).end();
+  const findPerson = persons.find(person => person.id === id);
+  findPerson ? res.json(findPerson) : res.status(404).end();
 });
 
 app.delete("/api/person/:id", (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter(person => person.id !== id);
-    res.status(204).end();
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
 });
 
 app.get("/api/info", (req, res) => {
-    const html = `
+  const html = `
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
     `;
-    res.send(html);
+  res.send(html);
 });
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
